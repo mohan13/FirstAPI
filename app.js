67c71d726cb711598e5e6eeac9fd582766404544
mohan13 <mohangrg250@gmail.com>
2023-01-07 23:06:31 +0545

    added 1.2

--- app.js
+++ app.js
@@ -37,16 +37,16 @@ app.use("/doc", swaggerUiExpress.serve, swaggerUiExpress.setup(swaggerDocs));
 app.use("/home", (req, res) => {
   res.status(200).json([
     {
-      banner: {
-        headline: "HOTEL & RESORT",
-        title: "Welcome to Roberto",
-      },
-      aboutUs: {
-        headline: "ABOUT US",
-        title: "Welcome to Roberto Hotel Luxury",
-        description:
-          "With over 340 hotels worldwide, NH Hotel Group offers a wide variety of hotels catering for a perfect stay no matter where your destination.",
-      },
+      headline: "HOTEL & RESORT",
+      title: "Welcome to Roberto",
+    },
+    {
+      headline: "ABOUT US",
+      title: "Welcome to Roberto Hotel Luxury",
+      description:
+        "With over 340 hotels worldwide, NH Hotel Group offers a wide variety of hotels catering for a perfect stay no matter where your destination.",
+    },
+    {
       testimonials: [
         {
           name: "Downey Sarah",
@@ -67,6 +67,8 @@ app.use("/home", (req, res) => {
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         },
       ],
+    },
+    {
       entertainment: [
         {
           headline: "Racing Bike",
